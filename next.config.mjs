380b9f2aa52aaa0c87898508fe7f9a5460c3ac19
higorsantana-omega <higor.santanahs17@gmail.com>
2024-03-10 14:59:26 -0300

    chore: config host and globals styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      hostname: 'wdc-icon-generator.s3.amazonaws.com'
+    }]
+  }
+};
 
 export default nextConfig;
